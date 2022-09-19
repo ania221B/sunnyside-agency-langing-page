2c20ca15325071675dcb311a027490ddbec425ba
ania221B <abuwaj@gmail.com>
2022-09-19 23:35:20 +0200

    clicking outside menu closes it

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ const menuBtn = document.querySelector('.js-menu-btn')
 menuBtn.addEventListener('click', e => {
   document.body.classList.toggle('menu-is-open')
 })
+
+document.body.addEventListener('click', e => {
+  if (document.body.classList.contains('menu-is-open')) {
+    if (!e.target.closest('.js-nav__list') && !e.target.closest('.js-menu-btn')) {
+      document.body.classList.remove('menu-is-open')
+    }
+  }
+})
